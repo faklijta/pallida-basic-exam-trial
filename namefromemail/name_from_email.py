3225c906a39f95127c8e06d98e27eddeffa361ec
faklijta <faklijta@gmail.com>
2017-09-19 10:18:57 +0200

    feat(trial exam) name from email complete

--- namefromemail/name_from_email.py
+++ namefromemail/name_from_email.py
@@ -4,5 +4,14 @@
 # last_name first_name
 # example: "elek.viz@exam.com" for this input the output should be: "Viz Elek"
 # accents does not matter
+# email = str(input("elek.viz@exam.com")
 
-print(name_from_email("elek.viz@exam.com"))
+def new_function(email):
+    name_split = email.split(".")
+    first_name = name_split[0]
+    name_split2 = name_split[1]
+    look_up_last_name = name_split2.split("@")
+    last_name = look_up_last_name[0]
+    name_from_email = (last_name).title() + " " + (first_name).title()
+    print(name_from_email)
+new_function("elek.viz@exam.com")
